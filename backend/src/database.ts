16c5c1ebab627a788df5e523f4949c18b3d62dd0
Arden X. <rnx@rnx.sh>
2025-06-25 18:42:26 -0400

    refactor(backend): move db related functions into a single class

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,4 +1,5 @@
-import sqlite3, { type Statement, type Database } from "better-sqlite3";
+import Sqlite3 from "better-sqlite3";
+import type { Statement, Database as Sqlite3Database } from "better-sqlite3";
 
 interface User {
   id: number;
@@ -13,56 +14,58 @@ interface Roll {
   user_id: number;
 }
 
-let db: Database;
-let findUserByUUIDStmt: Statement;
-let createUserWithUUIDStmt: Statement;
-let createRollForUserStmt: Statement;
-let getRollsByUserStmt: Statement;
+export default class Database {
+  private db: Sqlite3Database;
 
-export function initDatabase() {
-  db = sqlite3(process.env.DB_FILE);
+  private findUserByIDStmt: Statement;
+  private findUserByUUIDStmt: Statement;
+  private findRollsStmt: Statement;
+  private createUserStmt: Statement;
+  private createRollStmt: Statement;
 
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY,
-      uuid TEXT NOT NULL UNIQUE,
-      name TEXT NOT NULL
-    );
+  constructor(path: string) {
+    this.db = new Sqlite3(path);
+    this.db.exec(`
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        uuid TEXT NOT NULL UNIQUE,
+        name TEXT NOT NULL
+      );
 
-    CREATE UNIQUE INDEX IF NOT EXISTS idx_users_uuid
-    ON users(uuid);
-  `);
+      CREATE UNIQUE INDEX IF NOT EXISTS idx_users_uuid ON users (uuid);
 
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS rolls (
-      id INTEGER PRIMARY KEY,
-      value INTEGER NOT NULL,
-      timestamp INTEGER NOT NULL,
-      user_id INTEGER NOT NULL,
-      FOREIGN KEY (user_id) REFERENCES users(id)
-    );
-  `);
+      CREATE TABLE IF NOT EXISTS rolls (
+        id INTEGER PRIMARY KEY,
+        value INTEGER NOT NULL,
+        timestamp INTEGER NOT NULL,
+        user_id INTEGER NOT NULL REFERENCES users (id)
+      );
+    `);
 
-  findUserByUUIDStmt = db.prepare("SELECT * FROM users WHERE uuid = ?");
-  createUserWithUUIDStmt = db.prepare("INSERT INTO users(uuid, name) VALUES (?, ?) RETURNING *");
-  createRollForUserStmt = db.prepare("INSERT INTO rolls(value, timestamp, user_id) VALUES (?, ?, ?) RETURNING *");
-  getRollsByUserStmt = db.prepare("SELECT * FROM rolls WHERE user_id = ?");
+    this.findUserByIDStmt = this.db.prepare("SELECT * FROM users WHERE id = ?");
+    this.findUserByUUIDStmt = this.db.prepare("SELECT * FROM users WHERE uuid = ?");
+    this.findRollsStmt = this.db.prepare("SELECT * FROM rolls WHERE user_id = ?");
+    this.createUserStmt = this.db.prepare("INSERT INTO users (uuid, name) VALUES (?, ?) RETURNING *");
+    this.createRollStmt = this.db.prepare("INSERT INTO rolls (value, timestamp, user_id) VALUES (?, ?, ?) RETURNING *");
+  }
 
-  process.on("exit", () => db.close());
-}
+  findUserByID(id: number): User | null {
+    return this.findUserByIDStmt.get(id) as User | null;
+  }
 
-export function findUserByUUID(uuid: string): User {
-  return findUserByUUIDStmt.get(uuid) as User;
-}
+  findUserByUUID(uuid: string): User | null {
+    return this.findUserByUUIDStmt.get(uuid) as User | null;
+  }
 
-export function createUserWithUUID(uuid: string): User {
-  return createUserWithUUIDStmt.get(uuid, "test") as User;
-}
+  findRolls(userId: number): Roll[] {
+    return (this.findRollsStmt.all(userId) ?? []) as Roll[];
+  }
 
-export function createRollForUser(id: number): Roll {
-  return createRollForUserStmt.get(500, new Date().getTime(), id) as Roll;
-}
+  createUser(uuid: string, name: string): User {
+    return this.createUserStmt.get(uuid, name) as User;
+  }
 
-export function getRollsByUser(id: number): Roll[] {
-  return getRollsByUserStmt.all(id) as Roll[];
+  createRoll(value: number, timestamp: number, userId: number): Roll {
+    return this.createRollStmt.get(value, timestamp, userId) as Roll;
+  }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,19 @@
 import "dotenv/config";
-import express, { type Request, type Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { randomUUID } from "crypto";
-import { createRollForUser, createUserWithUUID, findUserByUUID, getRollsByUser, initDatabase } from "./database.js";
-
-initDatabase();
+import Database from "./database.js";
 
 const app = express();
 const port = process.env.PORT ?? "3000";
+const db = new Database(process.env.DB_FILE);
 
 app.use(cookieParser());
 
 app.get("/api/auth", async (req: Request, res: Response) => {
   const uuid = req.cookies.uuid;
-  const user = findUserByUUID(uuid);
+  const user = db.findUserByUUID(uuid);
 
   if (!user) {
     res.sendStatus(401);
@@ -24,38 +24,38 @@ app.get("/api/auth", async (req: Request, res: Response) => {
 
 app.post("/api/auth", async (req: Request, res: Response) => {
   let uuid = req.cookies.uuid;
-  let user = findUserByUUID(uuid);
+  let user = db.findUserByUUID(uuid);
   if (user) {
     res.status(200).send(user);
     return;
   }
 
   uuid = randomUUID();
-  user = createUserWithUUID(uuid);
+  user = db.createUser(uuid, "test");
   res.status(201).send(user);
 });
 
 app.get("/api/roll", async (req: Request, res: Response) => {
   const uuid = req.cookies.uuid;
-  const user = findUserByUUID(uuid);
+  const user = db.findUserByUUID(uuid);
   if (!user) {
     res.sendStatus(401);
     return;
   }
 
-  const rolls = getRollsByUser(user.id);
+  const rolls = db.findRolls(user.id);
   res.status(200).send(rolls);
 });
 
 app.post("/api/roll", async (req: Request, res: Response) => {
   const uuid = req.cookies.uuid;
-  const user = findUserByUUID(uuid);
+  const user = db.findUserByUUID(uuid);
   if (!user) {
     res.sendStatus(401);
     return;
   }
 
-  const roll = createRollForUser(user.id);
+  const roll = db.createRoll(500, new Date().getTime(), user.id);
   res.status(200).send(roll);
 });
 
